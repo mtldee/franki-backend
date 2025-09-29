3473e076754b8cbd18b3fa5c965e82facf3a398a
Matilde Galarce <matildegm@uc.cl>
2025-09-28 23:16:54 -0300

    fix front url

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 const cors = require("cors");
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "https://franki-vxof.onrender.com" }));
 
 app.get("/", (req, res) => {
   res.send("Servidor funcionando 🚀");
